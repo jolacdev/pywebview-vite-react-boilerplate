f1f9428949b309924233f9a0344d1125eedeb820
Jose L <58451072+jolacdev@users.noreply.github.com>
2025-10-30 17:42:10 +0100

    add eslint and prettier config

---
+++ frontend/.prettierrc.mjs
@@ -0,0 +1,11 @@
+export default {
+  arrowParens: 'always',
+  bracketSameLine: false,
+  endOfLine: 'auto',
+  jsxSingleQuote: false,
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+};
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsDir: '',
+    emptyOutDir: true,
+    outDir: '../frontend_dist',
+    sourcemap: false,
+  },
   server: {
     port: 3000,
-  },
-  build: {
-    outDir: "../frontend_dist",
-    assetsDir: "",
-    sourcemap: false,
-    emptyOutDir: true,
   },
 });
