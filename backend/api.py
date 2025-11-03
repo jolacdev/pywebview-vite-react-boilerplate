3afed355f27165885a855ca19bb87e92ae14d9a5
Jose L <58451072+jolacdev@users.noreply.github.com>
2025-11-03 19:15:03 +0100

    chore(lint): implement MyPy for static type checking
    - enable Ruff file-specific typing rules
    - fix errors reported by Ruff and MyPy

--- backend/api.py
+++ backend/api.py
@@ -1,20 +1,20 @@
 import numpy as np
 import webview
-from pyflow import extensity
+from pyflow import extensity  # type: ignore
 
 
 @extensity
 class PyWebViewApi:
     """Python API functions exposed to JavaScript."""
 
-    def generate_random_number_array(self, length: int = 4):
+    def generate_random_number_array(self, length: int = 4) -> list[float]:
         return np.random.rand(length).tolist()
 
-    def toggle_fullscreen(self):
+    def toggle_fullscreen(self) -> None:
         webview.windows[0].toggle_fullscreen()
 
-    def save_content(self, content):
-        if not (filename := webview.windows[0].create_file_dialog(webview.FileDialog.SAVE)):
+    def save_content(self, content: str) -> None:
+        if not (filenames := webview.windows[0].create_file_dialog(webview.FileDialog.SAVE)):
             return
-        with open(filename, "w") as f:
+        with open(filenames[0], "w") as f:
             f.write(content)
