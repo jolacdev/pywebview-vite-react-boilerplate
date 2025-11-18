7be68ab0431f0671b497485ce5e49978f18e63dc
Jose L <jolacdev@users.noreply.github.com>
2025-11-18 18:24:53 +0100

    feat(styles): implement custom theme colors and add Prettier plugin for Tailwind

--- frontend/.prettierrc.mjs
+++ frontend/.prettierrc.mjs
@@ -3,6 +3,7 @@ export default {
   bracketSameLine: false,
   endOfLine: 'auto',
   jsxSingleQuote: false,
+  plugins: ['prettier-plugin-tailwindcss'],
   printWidth: 80,
   semi: true,
   singleQuote: true,
